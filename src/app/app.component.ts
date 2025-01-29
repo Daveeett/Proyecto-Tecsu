827dbb076bf81b44aba9864469d9cbb7faceac20
ivo <ivadavila282003@gmail.com>
2025-01-28 23:35:19 -0500

    cambios de arquitectura + add de tailwind + hoja de estilos + nuevos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegistroComponent } from "./registro/registro.component";
+import { RegistroComponent } from "./components/registro/registro.component";
 import { AdminComponent } from "./admin/admin.component";
-import { RankingComponent } from './ranking/ranking.component';
-import { FooterComponent } from "./footer/footer.component";
+import { RankingComponent } from './components/ranking/ranking.component';
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistroComponent, AdminComponent, RankingComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
